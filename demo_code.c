341bb66b2662853e506415c79bff3bfc4faabea6
Ashish <ashishpr78@gmail.com>
2020-01-08 01:55:29 +0530

    "String Palindrome"

--- demo_code.c
+++ demo_code.c
@@ -10,14 +10,14 @@ printf("Enter a string\n");
 scanf("%s",s);
 printf("\n");
 int len = printf("%s",s);
-printf("len is %d",len);
+
 for(i=len-1;i>=0;i--)
 {
     rev_string[j]=s[i];
     j++;
 }
 rev_string[j]='\0';
-printf("\n reverse string is %s",rev_string);
+
 if(strcmp(s,rev_string)==0)
 {
     printf("\nString is Palindrome");
